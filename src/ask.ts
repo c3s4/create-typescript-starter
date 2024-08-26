1d5307558d8fb5bbcf475fef105d606b3f3b94e3
Davide Cesaroni <davide.cesaroni@gmail.com>
2024-08-26 17:12:14 +0200

    Start asking questions

---
+++ src/ask.ts
@@ -0,0 +1,44 @@
+import { input, select, Separator } from '@inquirer/prompts';
+
+export interface Answers {
+  projectName: string;
+  projectDescription: string;
+  projectAuthor: string;
+  projectVersion: string;
+  projectLicense: string;
+  projectRepository: string;
+  packageManager: string;
+}
+
+const Ask = async (): Promise<Answers> => {
+  const answers: Answers = {
+    projectName: await input({ message: 'What is the project name?', required: true }),
+    projectDescription: await input({ message: 'What is your project description (optional)?' }),
+    projectAuthor: await input({ message: 'What is your project author (optional)?' }),
+    projectVersion: await input({ message: 'What is your project version (optional)?', default: '1.0.0' }),
+    projectLicense: await input({ message: 'What is your project license (optional)?', default: 'MIT' }),
+    projectRepository: await input({ message: 'What is your project repository (optional)?' }),
+    packageManager: await select({
+      message: 'What package manager do you want to use (npm or yarn)?',
+      choices: [
+        {
+          name: 'npm',
+          value: 'npm',
+        },
+        {
+          name: 'yarn',
+          value: 'yarn',
+        },
+        {
+          name: 'pnpm',
+          value: 'pnpm',
+        },
+      ],
+      default: 'pnpm',
+    }),
+  };
+
+  return answers;
+};
+
+export { Ask };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello, world!');
+/*
+
+- Steps to be executed
+  1. Ask for project name
+  2. Ask for project description (optional)
+  3. Ask for project author (optional)
+  4. Ask for project version (optional)
+  5. Ask for project license (optional)
+  6. Ask for project repository (optional)
+  (7. Ask if is a library or an application, maybe not now)
+  7. Ask for package manager (npm or yarn)
+  8. Create project folder if not exists
+  9. Read template files and replace placeholders
+  10. Write files to project folder
+  11. Install dependencies adding 
+  12. Init git repository
+  13. Make initial commit
+  14. Ask for dependencies upgrade
+  15. Upgrade dependencies if requested
+  15. Done
+
+*/
+
+import { Ask } from './ask';
+
+const answers = await Ask();
+
+console.log(answers);
