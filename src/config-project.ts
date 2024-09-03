5b5fcaa7c73e9528ca4e0736b34f100dc8c86f1c
Davide Cesaroni <davide.cesaroni@gmail.com>
2024-09-03 18:04:55 +0200

    Fix issue for the right templates path

--- src/config-project.ts
+++ src/config-project.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import { Answers } from './ask';
 import { replacePlaceholders } from './helpers';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 // create project folder
 export const createProjectFolder = (projectName: string): void => {
@@ -18,20 +20,25 @@ export const copyTemplateFiles = async (answers: Answers): Promise<void> => {
   console.log(`Copying template files to project folder ${answers.projectName}...`);
   console.log();
 
+  const __filename = fileURLToPath(import.meta.url);
+  console.log(__filename);
+
   // check if directory is empty, if not exit and alert with an error message
 
   const isDirectoryEmpty = (await fs.promises.readdir(answers.projectName)).length === 0;
   if (!isDirectoryEmpty) {
     throw new Error(`Directory ${answers.projectName} is not empty.`);
   }
+  const templatesPath = path.join(path.dirname(__filename), '..', 'templates');
+  console.log(templatesPath, path.dirname(__filename));
 
-  const templateFiles = await fs.promises.readdir('templates');
+  const templateFiles = await fs.promises.readdir(templatesPath);
 
   console.log('Copying files...');
   console.log();
 
   const copyAndReplace = async (file: string) => {
-    const content = await fs.promises.readFile(`templates/${file}`, 'utf8');
+    const content = await fs.promises.readFile(path.join(templatesPath, file), 'utf8');
     console.log(`Copying ${file}...`);
     console.log();
     const replacedContent = replacePlaceholders(content, answers);
